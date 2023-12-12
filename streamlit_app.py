c57684370da4323b9de8715e87d64d1ab8ae6e07
vutranclan <tran.vu.dang.huy@gmail.com>
2023-12-12 18:41:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,21 @@ import pandas as pd
 import streamlit as st
 import streamlit_authenticator as stauth
 
+import yaml
+from yaml.loader import SafeLoader
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
 st.write('hallo huy')
 """
 # Welcome to Streamlit!
