16785f0a6b01b9998e065ebd488a54ca5aa5c3cb
vutranclan <tran.vu.dang.huy@gmail.com>
2023-12-12 18:45:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit_authenticator as stauth
 
 import yaml
 from yaml.loader import SafeLoader
-with open('../config.yaml') as file:
+with open('config.yaml') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 authenticator = Authenticate(
