8ec75a9cb56297a9e7ca158b053aa59ccf6057ac
vutranclan <tran.vu.dang.huy@gmail.com>
2023-12-12 18:43:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,18 @@ authenticator = Authenticate(
 
 name, authentication_status, username = authenticator.login('Login', 'main')
 
+
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+
+
 st.write('hallo huy')
 """
 # Welcome to Streamlit!
